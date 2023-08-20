b4768f033c2ff2d2d6682e1490a8137a82c34054
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-08-20 23:36:59 +0530

    added another route and controller

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ console.log('Router loaded');
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+//for any other routes, access from here
+//routes.use('/route', requier('routeFileName'));
 
 module.exports = router;
