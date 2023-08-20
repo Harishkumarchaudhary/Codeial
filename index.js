4faefaae74c7ea39f059c88cd0aff68ce7e45285
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-08-20 23:18:41 +0530

    set up routes and controllers

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 8000;
 
 const app = express();
 
+//Use express router
+app.use('/', require('./routes/index')); //We can simply state ./routes too that would automatically mean ./routes/index
+
 app.listen(port, function(err){
    if (err) {
       console.log(`Error in running the server: ${err}`);
