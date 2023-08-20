0c005721eed28bd45d7baa93fb71396fb7e2227a
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-08-20 23:49:43 +0530

    install ejs and use app.set

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 //Use express router
 app.use('/', require('./routes/index')); //We can simply state ./routes too that would automatically mean ./routes/index
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
    if (err) {
       console.log(`Error in running the server: ${err}`);
