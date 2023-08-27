5e0f8c77e463a77981d7d95c9da862ca2b05ccfe
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-08-27 18:23:23 +0530

    created layout.ejs and set up static files

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
 
 //Use express router
 app.use('/', require('./routes/index')); //We can simply state ./routes too that would automatically mean ./routes/index
