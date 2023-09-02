28f786799745a85ddb6267e1ac1451c912e8c118
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-02 17:43:06 +0530

    sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res) {
     //return res.end('<h1>User profile</h1>');
     return res.render('users', {
@@ -20,7 +22,26 @@ module.exports.signIn = function(req, res) {
 
 //get the sign up data
 module.exports.create = function(req, res) {
-    //ToDo later
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email})
+    .then((user)=>{
+        if (!user) {
+            User.create(req.body)
+            .then((user)=>{
+                console.log(user);
+                return res.redirect('/users/sign-in');
+            }).catch((err)=>{
+                console.log('error in creating user while signing up'); return;
+            });
+        } else {
+            return res.redirect('back');
+        }
+    }).catch((err)=>{
+        console.log('error in finding user while signing up'); return;
+    })
 }
 
 //sign in and create session for loggin in user
