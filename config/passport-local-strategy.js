52a1fd37c89620b103e8ba4451356bb218037316
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-03 21:40:19 +0530

    setting up passport.js

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+//authentication using passport
+passport.use(new LocalStrategy({
+           usernameField: 'email'
+      },
+      function(email, password, done) {
+          //find a user and establish the identity
+          User.findOne({email: email}).then((user)=>{
+
+            if (!user || user.password != password) {
+                console.log('Invalid user/pwd');
+                return done(null, false);
+            }
+
+            return done(null, user);
+              
+          }).catch((err)=>{
+                console.log('Error in finding the user--> Passport');
+                return done(err);
+          });
+      }
+));
+
+//serializing the user to decide which key is to be kept in cookies like we did in manual-auth
+passport.serializeUser(function(user, done) {
+          done(null, user.id); //We are encrypting userid in the cookie
+});
+
+
+
+//deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done) {
+       User.findById(id).then((user)=>{
+          return done(null, user);
+       }).catch((err)=>{
+          console.log('Error in finding the user--> Passport');
+          return done(err);
+       });
+});
+
+
+module.exports = passport;
