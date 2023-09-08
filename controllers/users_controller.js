605c9b3c24f44f09beb25c5a5bd5eaa92ac355c6
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-08 21:52:48 +0530

    passing data to views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,11 @@ module.exports.profile = function(req, res) {
 }
 
 module.exports.signUp = function(req, res) {
+
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: 'Codial | Sign Up'
     })
@@ -15,6 +20,12 @@ module.exports.signUp = function(req, res) {
 
 //render sign in page
 module.exports.signIn = function(req, res) {
+
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
+    
     return res.render('user_sign_in', {
         title: 'Codial | Sign In'
     })
