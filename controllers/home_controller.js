c93e497aa1690f05a51a61767c909cfac28b9e76
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-10 22:26:17 +0530

    display posts and populate user complete object in post result

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,27 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res) {
     // return res.end('<h1>Express is up for Codial</h1>')
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}).then((posts)=>{
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // }).catch({
+
+    // });
+
+
+    //populating whole user instead of just id
+    Post.find({}).populate('user').exec().then((posts)=>{
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
+    }).catch((err)=>{
+
     });
 }
