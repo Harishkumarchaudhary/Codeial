4916217a039cfc9e4f5a3f99133b28bd3234125c
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-10 23:12:04 +0530

    creating comment Schema

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+      content: {
+        type: String,
+        required: true
+      },
+      post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post' //refer to Post schema
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' //refer to user schema
+      }
+}, {
+        timestamps: true //This field will create createdAt and updatedAt in db. Mongoose does it for us
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -8,7 +8,15 @@ const postSchema = mongoose.Schema({
       user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User' //refer to user schema
-      }
+      },
+      //Include the array of ids of all
+      // the comments in this post schema because we need to access comments for a post very frequently
+      comments: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Comment' //refer to Comment schema
+        }
+      ]
 }, {
         timestamps: true //This field will create createdAt and updatedAt in db. Mongoose does it for us
 });
