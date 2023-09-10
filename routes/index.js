2007e215e922592843c31fd555387ad628aaf7dc
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-10 21:14:03 +0530

    saving posts to db

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 console.log('Router loaded');
 const homeController = require('../controllers/home_controller');
+const { route } = require('./users');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 //for any other routes, access from here
 //routes.use('/route', requier('routeFileName'));
