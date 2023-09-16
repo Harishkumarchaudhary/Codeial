ec7078e7d23b627c22804b2c832dfcdc9b77aa10
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-16 22:46:33 +0530

    updating users profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,6 +13,20 @@ module.exports.profile = function(req, res) {
     });
 }
 
+module.exports.update = function(req, res) {
+    if (req.user.id == req.params.id) {
+        User.findByIdAndUpdate(req.params.id, req.body)
+        .then((user)=>{
+             return res.redirect('back');
+        })
+        .catch((err)=>{
+
+        });
+    } else {
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 module.exports.signUp = function(req, res) {
 
     if (req.isAuthenticated()) {
