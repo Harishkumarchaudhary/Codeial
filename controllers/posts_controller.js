5e562f06d7837b0f840bf8c6caa94ee7092dcbe7
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-17 16:52:59 +0530

    conevrting more code to async/await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,39 +1,34 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res) {
-      Post.create({
-        content: req.body.content,
-        user: req.user._id
-      }).then((post)=>{
+module.exports.create = async function(req, res) {
+      try {
+        await Post.create({
+          content: req.body.content,
+          user: req.user._id
+        });
         return res.redirect('back');
-      })
-      .catch((err)=>{
-          console.log('Error in creating a post', err);
-      });
+      } catch(err) {
+        console.log('Error in creating a post', err);
+      }
 }
 
-module.exports.destroy = function(req, res) {
-     Post.findById(req.params.id).then((post)=>{
+module.exports.destroy = async function(req, res) {
+     try {
+      let post =  await Post.findById(req.params.id);
       //Only user who has written that post should be able to delete it
       //.id means converting the object id into string
       if (post.user == req.user.id) {
-         Post.findByIdAndDelete(req.params.id).then((post)=>{
-          Comment.deleteMany({post: req.params.id}).then(()=>{
-            console.log('Succesfully deleted post along with comments');
-            return res.redirect('back');
-          }).catch((err)=>{
-           console.log('Something went wrong', err);
-           return res.redirect('back');
-          });
-         }).catch((err)=>{
-
-         });
+        await Post.findByIdAndDelete(req.params.id);
+        await Comment.deleteMany({post: req.params.id});
+        console.log('Succesfully deleted post along with comments');
+        return res.redirect('back');
       } else {
          return res.redirect('back');
       }
-     }).catch((err)=>{
-       console.log('Something went wrong, post  not found', err);
-       return res.redirect('back');
-     });
+     } catch (err) {
+      console.log('Something went wrong, post  not found', err);
+      return res.redirect('back');
+     }
+
 }
