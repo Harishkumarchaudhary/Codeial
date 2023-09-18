b9d8289f82b899d8838e5dd8e881f41ed3261890
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-09-18 15:06:53 +0530

    inserting post in DOM

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,11 +10,44 @@
             data: newPostForm.serialize(), //converts form data into json
             success: function(data) {
                  console.log(data);
+                 let newPost = newPostDom(data.data.post);
+                 $("#posts-list-container>ul").prepend(newPost);
             }, error: function(err) {
                  console.log(err.responseText);
             }
           })
         });
+    }
+
+    let newPostDom = function (post) {
+         return $(`<li id="post-${post._id}">
+         
+                 <small>
+                 <a class="delete-post-button" href="/posts/destroy/${ post.id}">Delete</a>
+                 </small>
+        
+                 ${ post.content }
+         <br>
+         <small>
+         ${ post.user.name }
+         </small>
+         <div class="post-comments">
+             
+                 <form action="/comments/create" method="POST">
+                     <input type="text" name="content" placeholder="Type here to add comment..." required>
+                     <input type="hidden" name="post" value="${ post._id }">
+                     <input type="submit" value="Add Comment">
+                 </form>
+             
+         </div>
+     
+         <div class="post-comments-list">
+             <ul id="post-comments-${ post._id}">
+                 
+             </ul>
+         </div>
+     
+     </li>`);
     }
 
     //method to insert the post into DOM
