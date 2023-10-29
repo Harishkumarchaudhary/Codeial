dc1736848670d631d8e22c38a0562a4ed43ab476
Harishkumarchaudhary <harishkumar211998@gmail.com>
2023-10-30 00:42:26 +0530

    production environment variables setup

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,24 @@ const development = {
 }
 
 const production = {
-    name: 'production'
+    name: process.env.CODIAL_ENVIRONMENT,
+    asset_path: process.env.CODIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODIAL_SMTP_AUTH_USER,
+            pass: process.env.CODIAL_SMTP_AUTH_PASS
+        }
+    },
+    google_client_id: process.env.CODIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_url: process.env.CODIAL_GOOGLE_CALLBACK_URLA,
+    jwt_secret: process.env.CODIAL_JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.CODIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODIAL_ENVIRONMENT);
